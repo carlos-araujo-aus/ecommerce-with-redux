88d8b8a35a933e91d8d4b28fd0007ef2c2721010
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-06-25 17:19:40 +0800

    Shopping Cart Componen Added

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,18 +1,74 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice'; 
 import './ShoppingCart.css'; 
 
 const ShoppingCart = () => {
 
+  const dispatch = useDispatch(); // Dispatch actions to Redux store
+  const cartItems = useSelector(state => state.cart.cartItems) // Get cart items from Redux store
+  const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0)  //Iteration over cart items to calculate total amount from 0 to the total amount
+
+  const handleRemoveItem = itemId => {
+    dispatch (removeItemFromCart(itemId))
+  }
+
+  const handleClearCart = () => {
+    dispatch (clearCart())
+  }
+
+  const handleIncreaseQuantity = itemId => {
+    dispatch (increaseItemQuantity(itemId))
+  }
+
+  const handleDecreaseQuantity = itemId => {
+    dispatch (decreaseItemQuantity(itemId))
+  }
+  
   return (
     <>
-    <div className="shopping-cart">
-      <h2 className="shopping-cart-title">Shopping Cart</h2>
-      <ul className="cart-items">
-       
-      </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
-    </div>
-  
+    
+      <div className="shopping-cart">
+        <h2 className="shopping-cart-title">Shopping Cart</h2>
+        <ul className="cart-items">
+        {
+          cartItems.map(item => (
+            <li key={item.id} className= "cart-item">
+              <span>
+                {item.name} - ${item.price} - {item.quantity}
+              </span>
+              <div>
+                <button 
+                  className= "quantity-control-btn"
+                  onClick= {() => handleDecreaseQuantity(item.id)}>              
+                  -
+                </button>
+                <button
+                  className= "quantity-control-btn"
+                  onClick= {() => handleIncreaseQuantity(item.id)}>
+                  +
+                </button>
+              </div>
+              <button
+                className= "remove-item-btn"
+                onClick= {() => handleRemoveItem(item.id)}>
+                Remove
+              </button>
+            </li>
+          ))
+        }
+        </ul>
+        <button 
+          className="clear-cart-btn"
+          onClick={handleClearCart}>
+          Clear Cart
+        </button>
+        <div>
+          {
+            totalAmount ? <div className="cart-total">Total Amount: {totalAmount}</div> : ""
+          }
+        </div>
+      </div>
     </>
   );
 };
--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import cartReducer from './Components/CartSlice';
 const store = configureStore({
     reducer: {
         cart: cartReducer
-    }
+    }  
 })
 
 export default store;
