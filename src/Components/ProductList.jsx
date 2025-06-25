a687a179c12ba63682573db46be32b955fcdda66
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-06-25 15:05:48 +0800

    productlist component added

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems)
+
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -9,18 +15,30 @@ const ProductList = () => {
     { id: 3, name: 'Product C', price: 30 },
   ];
 
+  const handleAddToCart = (product) => {
+    dispatch(addItemToCart(product))
+  }
+
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-        {
-          products.map((product) => (
+        {products.map((product) => {
+          const isAlreadyInCart = cartItems.some(item => item.id === product.id)
+
+          return (
             <li key={product.id} className= "product-list-item">
               <span>{product.name} - ${product.price}</span>
-              <button>Add to Cart</button>
+              <button 
+                className={`add-to-cart-btn ${isAlreadyInCart ? 'disabled' : ''}`}
+                onClick={() => handleAddToCart(product)}
+                disabled={isAlreadyInCart}
+                >
+                {isAlreadyInCart ? 'Added' : 'Add to Cart'}
+              </button>
             </li>
-          ))
-        }
+          )       
+        })}
       </ul>
     </div>
   );
